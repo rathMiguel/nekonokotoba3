1eff300817795309916ab2104e6ff6a8e6886fb9
rath_miguel <rath.miguel@gmail.com>
2024-01-10 21:45:11 +0900

    add: 投稿リストapiに絞り込み機能を追加

--- server/api/notion/posts.get.ts
+++ server/api/notion/posts.get.ts
@@ -1,12 +1,35 @@
 import { Client } from "@notionhq/client"
-import { type H3Event } from 'h3'
+import type { H3Event } from 'h3'
+
+interface postsQueryList {
+  slug: string
+}
 
 export default defineEventHandler((event: H3Event) => {
   const notion: Client = new Client({ auth: process.env.NOTION_TOKEN })
-  const databaseId: string = process.env.NOTION_DATABASE_ID 
+  const databaseId: string = process.env.NOTION_DATABASE_ID
+
+  const initQuery: postsQueryList = {
+    slug: ''
+  }
+
+  const queryEventParams: Object = getQuery(event)
+  const allQueryParams: postsQueryList = Object.assign(initQuery, queryEventParams)
+
+  const { slug } = allQueryParams
 
   const response = notion.databases.query({
     database_id: databaseId,
+    filter: {
+      and: [
+        {
+          property: "slug",
+          rich_text: {
+            equals: slug
+          }
+        }
+      ]
+    }
   })
 
   return response
