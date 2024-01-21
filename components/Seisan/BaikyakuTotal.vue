95229ab34b2670753acdc3b02bbf3af18ac742f5
rath_miguel <rath.miguel@gmail.com>
2024-01-21 17:53:27 +0900

    add: クリップボードにコピー機能の追加

--- components/Seisan/BaikyakuTotal.vue
+++ components/Seisan/BaikyakuTotal.vue
@@ -8,6 +8,13 @@
 
     return nonEmptyAmounts.reduce((x, y) => x + y, 0)
   })
+
+  const copyToClipboard = (text: string) => {
+    return navigator.clipboard.writeText(text)
+      .then(() => { console.log(`copy to Clipboard "${text}"`) })
+      .catch(e => { console.error(e) })
+  }
+
 </script>
 
 <template>
@@ -16,15 +23,17 @@
       <dl class="flex mb-2">
         <dt>合計額：</dt>
         <dd>
-          <span class="cursor-pointer border-b-2 px-1" title="クリップボードにコピー">{{ aomoutsTotal.toLocaleString() }}</span>
-          <span> zeny</span>
+          <span class="cursor-pointer border-b-2 px-1" title="クリップボードにコピー" v-on:click="copyToClipboard(aomoutsTotal)">{{ aomoutsTotal.toLocaleString() }}</span>
+          <span class="ml-1">zeny</span>
         </dd>
       </dl>
       <dl class="flex">
         <dt>一人あたりの受取額：</dt>
         <dd>
-          <span class="cursor-pointer border-b-2 px-1" title="クリップボードにコピー">{{ (Math.floor(aomoutsTotal / seisanState.member)).toLocaleString() }}</span>
-          <span> zeny</span>
+          <span class="cursor-pointer border-b-2 px-1" title="クリップボードにコピー" v-on:click="copyToClipboard(Math.floor(aomoutsTotal / seisanState.member))">
+            {{ (Math.floor(aomoutsTotal / seisanState.member)).toLocaleString() }}
+          </span>
+          <span class="ml-1">zeny</span>
           <div class="-ml-2"><small class="ml-2 text-xs">※小数点以下は切り捨て</small></div>
         </dd>
       </dl>
--- components/Seisan/Member.vue
+++ components/Seisan/Member.vue
@@ -9,7 +9,7 @@
       <h1 class="mb-2">1. PT人数を入力</h1>
     </header>
     <div class="w-40 flex items-center">
-      <input type="number" min="0" max="12" step="1" class="w-full border-2 rounded-lg p-2 border-primary" v-model="seisanState.member">
+      <input type="number" min="1" max="12" step="1" class="w-full border-2 rounded-lg p-2 border-primary" v-model="seisanState.member">
       <span class="inline-flex ml-3">人</span>
     </div>
   </section>
