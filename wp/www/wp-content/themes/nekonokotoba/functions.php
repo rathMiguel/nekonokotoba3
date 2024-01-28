125ffbb342a59a0cb92ca8a8b6e5d2a687dbcf18
rath_miguel <rath.miguel@gmail.com>
2024-01-28 18:25:51 +0900

    add: 管理画面の制御

---
+++ wp/www/wp-content/themes/nekonokotoba/functions.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * 【管理画面】投稿メニューを非表示
+ */
+
+function remove_menus () {
+  global $menu;
+  remove_menu_page( 'edit.php' );
+}
+add_action('admin_menu', 'remove_menus');
+
+/**
+ * 固定ページのエディタを無効化
+ */
+
+ function remove_post_support() {
+  remove_post_type_support('page','comments');
+  remove_post_type_support('page','trackbacks');
+}
+add_action('init','remove_post_support');
