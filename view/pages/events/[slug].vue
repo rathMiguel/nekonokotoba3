d2eb3c76e069c03badef6dd2f46b313259f88fbf
rath_miguel <rath.miguel@gmail.com>
2024-02-11 16:04:47 +0900

    update: イベント用にファイル作成

---
+++ view/pages/events/[slug].vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  const config = useRuntimeConfig()
+  const route = useRoute('data-slug')
+
+  const { data } = await useFetch(`${config.public.WP_API_URL}pages/?slug=${route.params.slug}`)
+</script>
+
+<template>
+  <Head>
+    <Title>{{ data[0].title.rendered }} - 猫のことば研究所</Title>
+    <Meta name="description" :content="data[0].acf.description" />
+    <Meta name="keywords" :content="data[0].acf.keywords" />
+  </Head>
+  <article>
+    <header>
+      <BlockH1>{{ data[0].title.rendered }}</BlockH1>
+    </header>
+    <WPContent :data="data[0]" />
+  </article>
+</template>
