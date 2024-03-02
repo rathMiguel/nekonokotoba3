fa713f0663d1e865bf9fb6ed1096cbdd8f431cf9
rath_miguel <rath.miguel@gmail.com>
2024-03-03 02:05:24 +0900

    fix: ファビコン、ogp周りの取り込み

--- view/nuxt.config.ts
+++ view/nuxt.config.ts
@@ -18,6 +18,19 @@ export default defineNuxtConfig({
           href: 'https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Zen+Kurenaido&display=swap',
           crossorigin: '',
         },
+        {
+          rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
+        },
+        { rel: 'apple-touch-startup-image', media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/iphone5_splash.png' },
+        { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/iphone6_splash.png' },
+        { rel: 'apple-touch-startup-image', media: '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)', href: '/splashscreens/iphoneplus_splash.png' },
+        { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)', href: '/splashscreens/iphonex_splash.png' },
+        { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/iphonexr_splash.png' },
+        { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)', href: '/splashscreens/iphonexsmax_splash.png' },
+        { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/ipad_splash.png' },
+        { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/ipadpro1_splash.png' },
+        { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/ipadpro3_splash.png' },
+        { rel: 'apple-touch-startup-image', media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/ipadpro2_splash.png' },
       ],
       htmlAttrs: {
         lang: 'ja'
@@ -30,7 +43,11 @@ export default defineNuxtConfig({
         {
           name: 'viewport',
           content: 'width=device-width, initial-scale=1'
-        }
+        },
+        { name: 'mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+        { name: 'apple-mobile-web-app-title', content: '猫のことば研究所' }
       ],
     },
   },
