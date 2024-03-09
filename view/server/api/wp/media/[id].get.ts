b4d574656086fe49eb881095e39745c977b5c1a7
rath_miguel <rath.miguel@gmail.com>
2024-03-09 17:01:52 +0900

    fix: プレビュー時、画像が表示されないのを解消

--- view/server/api/wp/media/[id].get.ts
+++ view/server/api/wp/media/[id].get.ts
@@ -1,10 +1,16 @@
 import { type H3Event } from 'h3'
 
 export default defineEventHandler(async (event: H3Event) => {
-
+  const { preview } = getQuery(event)
+  const headers = preview ? {
+    Authorization : `Basic ${process.env.WP_PREVIEW_KEY}`
+  } : undefined
   if(event.context.params === undefined) return
+
   const imgId = event.context.params.id
-  const response: Response = await fetch(`${process.env.WP_API_URL}media/${imgId}`)
+  const response: Response = await fetch(`${process.env.WP_API_URL}media/${imgId}`, {
+    headers
+  })
   
   return await response.json()
 })
